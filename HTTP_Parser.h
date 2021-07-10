bebd106c8fed2fbea4658c22cd854af5c6b03938
yuesong-feng <yuesong-feng@foxmail.com>
2021-07-10 21:36:36 +0800

    update

--- HTTP_Parser.h
+++ HTTP_Parser.h
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <string>
 #include <unordered_map>
 #include <sstream>
@@ -6,15 +5,13 @@
 class HTTP_Parser
 {
 private:
-    std::unordered_map<std::string, std::string> http_package;
+    std::unordered_map<std::string, std::string> http;
 
 public:
-    HTTP_Parser();
     HTTP_Parser(char *buf);
     ~HTTP_Parser();
     void show();
     std::string operator[](std::string str);
-    void set(std::string key, std::string value);
 };
 
 HTTP_Parser::HTTP_Parser(char *buf)
@@ -22,57 +19,80 @@ HTTP_Parser::HTTP_Parser(char *buf)
     std::string buf_string = buf;
     std::istringstream buf_stream(buf_string);
     std::string line;
-    for (int i = 0; getline(buf_stream, line); ++i)
+    enum parts
     {
-        if (i == 0)
+        request_response,
+        header,
+        body
+    };
+    parts part = request_response;
+    std::string body_string;
+    while (getline(buf_stream, line))
+    {
+        switch (part)
+        {
+        case request_response:
         {
             std::istringstream line_stream(line);
             std::string tmp;
             line_stream >> tmp;
-            http_package.insert(std::make_pair("Method", tmp));
-            line_stream >> tmp;
-            http_package.insert(std::make_pair("Path", tmp));
-            line_stream >> tmp;
-            http_package.insert(std::make_pair("Protocol", tmp));
+            if (tmp.find("HTTP") == std::string::npos)
+            {
+                http.insert(std::make_pair("method", tmp));
+                line_stream >> tmp;
+                http.insert(std::make_pair("path", tmp));
+                line_stream >> tmp;
+                http.insert(std::make_pair("protocol", tmp));
+            }
+            else
+            {
+                http.insert(std::make_pair("protocol", tmp));
+                line_stream >> tmp;
+                http.insert(std::make_pair("status", tmp));
+                line_stream >> tmp;
+                http.insert(std::make_pair("status_msg", tmp));
+            }
+            part = header;
+            break;
         }
-        else
+        case header:
         {
-            auto pos = line.find(": ");
-            if (pos == std::string::npos)
-                continue;
+            if (line.size() == 1)
+            {
+                part = body;
+                break;
+            }
+            auto pos = line.find(":");
             std::string tmp1(line, 0, pos);
             std::string tmp2(line, pos + 2);
-            http_package.insert(std::make_pair(tmp1, tmp2));
+            http.insert(std::make_pair(tmp1, tmp2));
+            break;
+        }
+        case body:
+            body_string.append(line);
+            body_string.append("\n");
+            break;
+        default:
+            break;
         }
     }
+    http.insert(std::make_pair("body", body_string));
 }
+
 HTTP_Parser::~HTTP_Parser()
 {
 }
 
 void HTTP_Parser::show()
 {
-    for (auto it = http_package.cbegin(); it != http_package.cend(); ++it)
+    for (auto unordered_map_it = http.cbegin(); unordered_map_it != http.cend(); ++unordered_map_it)
     {
-        std::cout << it->first << ": " << it->second << std::endl;
+        std::cout << unordered_map_it->first << ": " << unordered_map_it->second << std::endl;
     }
 }
 
 std::string HTTP_Parser::operator[](std::string str)
 {
-    auto it = http_package.find(str);
-    return it != http_package.end() ? it->second : "";
-}
-
-void HTTP_Parser::set(std::string key, std::string value)
-{
-    auto it = http_package.find(key);
-    if (it == http_package.end())
-    {
-        http_package.insert(std::make_pair(key, value));
-    }
-    else
-    {
-        it->second = value;
-    }
+    auto it = http.find(str);
+    return it != http.end() ? it->second : "";
 }
